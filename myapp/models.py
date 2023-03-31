b4613f1f41599b13d59f8a3cb3aac4fd3ef03144
m0njurul <monjurul15-10804@diu.edu.bd>
2023-03-31 17:01:31 +0600

    second commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(default="+880", max_length=14)
+    date_time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    date_time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
